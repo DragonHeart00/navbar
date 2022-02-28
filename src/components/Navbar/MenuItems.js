02ba0d66d743190ade11e7365d185c69dd7f0f3e
Dragon <s195462@student.dtu.dk>
2022-02-28 11:39:38 +0100

    added menu

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+
+export const MenuItems = [
+    {
+        title: 'Forside',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Menu',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Om os',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Kontakt os',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName:'nav-links-mobile'
+    }
+]
